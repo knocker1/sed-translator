f6c1f32c73f0b35896afcef1549f957ba9942e44
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-12-04 22:14:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,31 @@ from telebot import types
 import config
 import translator as tr
 import database as db
+import asyncio
+
 
 bot = telebot.TeleBot(config.telegram_token)
+
+
+# Check if user has admin rights
+async def is_register_admin(chat, user, client):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await client(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
 
 
 @bot.message_handler(commands=['start'])
